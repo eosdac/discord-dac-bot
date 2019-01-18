dc899c595744d28b811119befcbd265937b69b40
unknown <kasperkasperfish@gmail.com>
2019-01-18 22:06:49 +0100

    implementation of pair and verification of eos account

--- commands/verify.js
+++ commands/verify.js
@@ -7,7 +7,34 @@ class cmd extends Base_Command{
     }
 
     async execute(bot, member, message, args){
-        message.author.send('Not yet implemented');
+
+            //check if discord id is in database
+            let discorduser = await bot.db.collection('disordbot').find({_id: message.author.id}).toArray();
+        
+            if(!discorduser.length) {message.author.send(`Please run the command \`$pair <accountname>\` first.`); return;}
+    
+            let actions = await this.eos.getActions(discorduser[0].eos_account);
+            let last = actions.find(a => { 
+                return a.act.account === bot.config.dac.verification_contract; 
+            });
+    
+            if(!last) {
+                message.author.send(`I couldn't find your on chain verification message. Please run the command again or verify your token again ${bot.config.dac.memberclient}/test/${discorduser[0].token}`);
+                return;
+            }
+    
+            if(last.act.data.token === discorduser[0].token ){
+                let guild = bot.client.guilds.find(guild => guild.name === bot.config.bot.guildname)
+                let role = guild.roles.find(role => role.name === "Registered Member");
+                member.addRole(role).catch(e=>console.error(e) );
+                message.author.send('I added the "Registered Member" role to your account.');
+            }
+            else{
+                message.author.send(`Tokens don't match, verification failed. You need to verify your token ${bot.config.dac.memberclient}/test/${discorduser[0].token}`);
+            }
+            
+        
+
     }
 }
 
