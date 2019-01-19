c2845990c54c86f9098bfe3b8c41c48c69aff7ef
unknown <kasperkasperfish@gmail.com>
2019-01-19 23:10:01 +0100

    send msg to disc channel with api call

--- api/routes.js
+++ api/routes.js
@@ -2,7 +2,8 @@
 const botRouter = function (api, bot) {
     const config = bot.config;
     
-    api.get("/config", async function (req, res) {
+    api.get("/testmsg", async function (req, res) {
+            await bot.client.channels.get('533715309803339780').send('this is a message send to the channel triggered over http');
             res.status(200).send(JSON.stringify(bot.config) );
     });
 
